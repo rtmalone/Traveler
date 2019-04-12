849ab944f97061da097c3e2a8708a2615d354fe9
Tyler Malone <tyler@pylon.com>
2019-04-12 00:11:59 -0400

    calculate winner util

---
+++ utils/calculateWinnter.js
@@ -0,0 +1,22 @@
+export default function calculateWinner(squares) {
+  const solutions = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 4, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    [0, 5, 10, 15],
+    [3, 6, 9, 12]
+  ]
+
+  for (let i = 0; i < solutions.length; i++) {
+    const [a, b, c, d] = solutions[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
