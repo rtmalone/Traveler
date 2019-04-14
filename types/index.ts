adaa94b3198549b87f1af6e5f549541e3a24eb1f
Tyler Malone <tyler@pylon.com>
2019-04-13 23:59:04 -0400

    Various data sets for Icons

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,10 @@ export interface GridItemType {
   name: string
   code: string
 }
+
+export enum IconSets {
+  FA = 'FONT_AWESOME',
+  FA5 = 'FONT_AWESOME_5',
+  MCI = 'MATERIAL_COMMUNITY_ICONS',
+  ENTYPO = 'ENTYPO'
+}
